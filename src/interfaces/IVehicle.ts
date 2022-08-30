82008383509545c75a44ec21f4a3d6350739a0f1
thales-bpl <thales.bplima@gmail.com>
2022-08-30 01:00:34 -0300

    impl: IVehicle interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const VehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
+
+export type IVehicle = z.infer<typeof VehicleSchema>;
+
+export default VehicleSchema;
+
+export { VehicleSchema };
