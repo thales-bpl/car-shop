884a99c5ca3823afd52f9489b991a02765e16086
thales-bpl <thales.bplima@gmail.com>
2022-08-30 20:35:22 -0300

    impl: CarService CRUD

---
+++ src/services/CarService.ts
@@ -0,0 +1,46 @@
+import { isValidObjectId, Model, updateQuery } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+import { ICar, carSchema } from '../interfaces/ICar';
+
+class CarService implements IService<ICar> {
+  private _carModel: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._carModel = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = carSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+
+    return this._carModel.create(obj);
+  }
+
+  public async read(): Promise<ICar[]> {
+    const allCars = await this._carModel.read();
+    return allCars;
+  }
+
+  public async readOne(_id: string): Promise<ICar | null> {
+    const targetCar = await this._carModel.readOne(_id);
+    if (!targetCar) throw new Error();
+    return targetCar;
+  }
+
+  public async update(_id: string, obj: ICar): Promise<ICar | null> {
+    const parsed = carSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+    const updatedCar = await this._carModel.update(_id, obj);
+    if (!updatedCar) throw new Error();
+    return updatedCar;
+  }
+
+  public async delete(_id: string): Promise<ICar | null> {
+    const deletedCar = await this._carModel.delete(_id);
+    if (!deletedCar) throw new Error();
+    return deletedCar;
+  }
+}
+
+export default CarService;
