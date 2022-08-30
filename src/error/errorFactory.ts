e9ab4311e3a1dd3f7a2ec938159384eacfb73872
thales-bpl <thales.bplima@gmail.com>
2022-08-30 03:27:06 -0300

    impl: error tools

---
+++ src/error/errorFactory.ts
@@ -0,0 +1,10 @@
+class ErrorFactory extends Error {
+  public status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
+
+export default ErrorFactory;
---
+++ src/error/errorHandler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import ErrorFactory from './errorFactory';
+
+const errorHandler = (
+  err: ErrorFactory,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  console.error(err.stack);
+
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
+
+export default errorHandler;
